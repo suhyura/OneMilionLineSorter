ed18b2fecf70ac897b19d313a434a35792e343d6
Yuriy Sukhov <suhyura@gmail.com>
2023-10-18 23:08:28 +0200

    refactor, split sigle big file to equal parts

---
+++ OneMilionLineSorter/Generator.cs
@@ -0,0 +1,43 @@
+﻿internal class Generator
+{
+    private readonly Random random = new Random();
+    private readonly string[] words;
+    public Generator()
+    {
+        words = GenerateWords();
+    }
+
+    private string[] GenerateWords()
+    {
+        return Enumerable.Range(0, 10_000)
+                            .Select(i => new string(
+                                         Enumerable.Range(0, random.Next(20, 100))
+                                            .Select(z => (char)random.Next('A', 'Z')).ToArray()
+                                            )
+                            ).ToArray();
+    }
+
+    public string Generate(int linesCount)
+    {
+        var fileName = $"Lines {linesCount}.txt";
+
+        using (var writer = new StreamWriter(fileName))
+        {
+            for (int i = 0; i < linesCount; i++)
+            {
+                writer.WriteLine(GenerateNumer() + ". " + GenerateString());
+            }
+        }
+        return fileName;
+    }
+
+    private string GenerateString()
+    {
+        return words[random.Next(0, words.Length)];
+    }
+
+    private string GenerateNumer()
+    {
+        return random.Next(0, 10_000).ToString();
+    }
+}
--- OneMilionLineSorter/Program.cs
+++ OneMilionLineSorter/Program.cs
@@ -2,44 +2,56 @@
 {
     private static void Main(string[] args)
     {
-        new Generator().Generate(1000);
+        var fileName = new Generator().Generate(10_000);
+        new Sorter().Sort(fileName, 1000);
         Console.WriteLine("Hello, World!");
     }
-}
-class Generator
-{
-    private readonly Random random = new Random();
-    private readonly string[] words;
-    public Generator()
+    class Sorter
     {
-        words = Enumerable.Range(0, 10_000)
-            .Select(i => new string(
-                         Enumerable.Range(0, random.Next(20, 100))
-                            .Select(z => (char)random.Next('A', 'Z')).ToArray()
-                            )
-            ).ToArray();
-
-    }
-    public void Generate(int linesCount)
-    {
-        var fileName = $"Lines {linesCount}.txt";
-
-        using (var writer = new StreamWriter(fileName))
+        public void Sort(string filename, int partLinesCount)
         {
-            for (int i = 0; i < linesCount; i++)
+            var files = SplitFile(filename, partLinesCount);
+            SortParts(files);
+            SortResult(files);
+
+        }
+
+        private void SortResult(string[] files)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SortParts(string[] files)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string[] SplitFile(string filename, int partLinesCount)
+        {
+            var parts = new List<string>();
+            using (StreamReader sr = new StreamReader(filename))
             {
-                writer.WriteLine(GenerateNumer() + ". " + GenerateString());
+                int partNumber = 0;
+                while (!sr.EndOfStream)
+                {   
+                    partNumber++;
+                    string partFileName = partNumber + ".txt";
+                    parts.Add(partFileName);
+                    using (var writer = new StreamWriter(partFileName))
+                    {
+
+                        for (int i = 0; i < partLinesCount; i++)
+                        {
+                            if (sr.EndOfStream)
+                                break;
+                            writer.WriteLine(sr.ReadLine());
+                        }
+                    }
+
+                }
             }
+            return parts.ToArray();
         }
     }
 
-    private string GenerateString()
-    {
-        return words[random.Next(0,words.Length)];
-    }
-
-    private string GenerateNumer()
-    {
-        return random.Next(0, 10_000).ToString();
-    }
 }
